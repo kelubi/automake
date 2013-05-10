8d63b2293ef061513f08c657f0bffd56063f07de
Ken <xuqinyong@gmail.com>
2013-05-10 19:07:16 +0800

    remove rpm

--- rpmbuild/init.sh
+++
@@ -1,5 +0,0 @@
-RPMBUILD_PAT=/root/lnmpp/rpmbuild
-echo "%_topdir   $RPMBUILD_PAT" > ~/.rpmmacros
-mkdir -p $RPMBUILD_PAT/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-mkdir -p $RPMBUILD_PAT/RPMS/{noarch,i386,i686,x86_64}
-find $RPMBUILD_PAT
